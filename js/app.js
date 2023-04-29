d7f3078e5a993339121c75ce97e9abeb810d27bd
malay009-star <117511789+malay009-star@users.noreply.github.com>
2023-04-29 18:53:21 +0500

    project has been changed

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var burger = document.getElementById('nav-burger');
+var menu = document.getElementById('menu');
+
+burger.addEventListener('click', () => {
+    menu.classList.toggle("is-open");
+})
